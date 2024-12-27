73edb003f3182081822accc4e19fdfeb7c9d305a
Dream-test <stsiapkou@gmail.com>
2024-12-27 19:15:56 +0100

    Create parsing in Main class

---
+++ src/main/java/org/webproject/Main.java
@@ -0,0 +1,102 @@
+package org.webproject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static java.lang.Thread.sleep;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //1. Указание пути к chromedriver.exe если не указан в Path системы
+        //System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\chromedriver\\chromedriver.exe");
+
+        //2. Создание объекта драйвер, который будет взаимодействовать с chromedriver
+        WebDriver driver = new ChromeDriver();
+
+        //3. Команда драйверу открыть браузер и загрузить указанный ресурс
+        driver.get("https://demoqa.com/books");
+
+        //4. Пауза между выполняемыми шагами
+        /*try {
+            sleep(10000);
+        } catch (InterruptedException exception) {
+            System.out.println(exception.getMessage());
+        } */
+
+        //5. Вариант получения строк с фильтрацией от пустых (исключая -padRow в названии класса) без использования промежуточного tableRowsIncludingHeader
+        //List<WebElement> filteredTableRows = driver.findElements(By.cssSelector(".rt-tr:not(.padRow)"));
+
+        //5. Вариант с промежуточным List<WebElement> tableRowsIncludingHeader
+        List<WebElement> tableRowsIncludingHeader = driver.findElements(By.className("rt-tr"));
+        List<WebElement> filteredTableRows = tableRowsIncludingHeader.stream()
+                //Три варианта реализации фильтра (убираю устаревшие классы и учитываю возможность генерации исключений)
+                //.filter(row -> !row.getAttribute("class").contains("-padRow"))
+                //.filter(row -> !row.getDomProperty("className").contains("-padRow"))
+                .filter(row -> !Objects.requireNonNull(row.getDomProperty("className")).contains("-padRow"))
+                .toList();
+
+        //6. Вывод на экран первого ряда таблицы (table header) где в ячейках только текстовое содержание.
+        //Определяю разделитель
+        final String infoDelimiter = " - ";
+
+        //получаю WebElement - первая строка таблицы
+        String headerValues = filteredTableRows.getFirst()
+                .findElements(By.className("rt-resizable-header-content"))
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.joining(infoDelimiter));
+
+        System.out.println(headerValues);
+
+
+
+        //7. Строки таблицы с информацией по книгам
+        filteredTableRows.stream().skip(1) //Стрим пропускает skip(1) первую строку из filterTableRows
+                .map(tableRow -> {
+                    String baseUrl = driver.getCurrentUrl();
+                    URI baseUri = URI.create(baseUrl);
+
+                    String relativeImgSrc = tableRow.findElement(By.tagName("img")).getDomAttribute("src");
+                    URI fullImgSrc = baseUri.resolve(Objects.requireNonNull(relativeImgSrc));
+                    String imgSrc = fullImgSrc.toString();
+
+                    WebElement bookCell = tableRow.findElement(By.tagName("a"));
+
+                    String relativeBookLink = bookCell.getDomAttribute("href");
+
+
+                    URI fullBookLinkUri = baseUri.resolve(Objects.requireNonNull(relativeBookLink));
+                    String bookLink = fullBookLinkUri.toString();
+                    String bookTitle = bookCell.getText();
+
+                    //ячейки автор и издатель можно идентифицировать только по имени тега и индексу
+                    final int authorCellIndex = 2;
+                    String bookAuthor = tableRow.findElements(By.className("rt-td")).get(authorCellIndex).getText();
+
+                    final int publisherCellIndex = 3;
+                    //String bookPublisher = tableRow.findElements(By.className("rt-td")).get(publisherCellIndex).getText();
+
+                    //Вариант поиска элемента в tableRow с использованием индекса и относительного xPath - точка означает поиск вглубь элемента tableRow
+                    String publisherCellXpath = String.format(".//div[%d]", publisherCellIndex + 1);
+                    String bookPublisher = tableRow.findElement(By.xpath(publisherCellXpath)).getText();
+
+                    //Соединение полученных строковых переменных в одну строку
+                    String tableRowInfo = String.join(infoDelimiter, imgSrc, "( " + bookLink + infoDelimiter + bookTitle + " )", bookAuthor, bookPublisher);
+                    return tableRowInfo;
+                })
+                .forEach(System.out::println);
+
+        //Закрываю процесс chromedriver.exe
+        driver.quit();
+    }
+}
---
+++ src/test/java/otg/webproject/WebQaTests.java
@@ -0,0 +1,4 @@
+package otg.webproject;
+
+public class WebQaTests {
+}
